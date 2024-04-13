7785526e1603246cbddc713fba399c08a75e3ee2
BabyWaffles <57724817+BabyWaffles@users.noreply.github.com>
2024-04-13 22:09:51 +0800

    Update framesplit_v0.1.0.py

--- framesplit_v0.1.0.py
+++ framesplit_v0.1.0.py
@@ -4,13 +4,18 @@ from logging import info, error
 from threading import Thread
 from asyncio import gather
 
-class global_frame_count:
-    def __init__(self, framecount):
-        return self.global_frame_count + framecount
-
-
 class systemRecurrsiveNull:
     pass
+
+class processingNullFrames:
+    pass
+
+class global_framecount:
+    def __init__(self, framecount):
+        self.framecount = framecount
+    def up_framecount(self):
+        self.framecount += 1
+        return self.framecount
 
 class GetCWD:
     def __init__(self, folder_name):
@@ -25,12 +30,41 @@ class GetCWD:
                 return makedirs(self.joindirw), 
             elif self.fname == None:
                 raise systemRecurrsiveNull(f"No directory name provided!")
-                return 0x0811
             else:
                 return self.joindirw
         except error as oserr:
             oserr(f"Error creating directory: {oserr}")	
             return None
         except systemRecurrsiveNull as srn:
-            raise systemRecurrsiveNull(f"Recursively returned NULL w/ err: {srn}")	
+            raise systemRecurrsiveNull(f"Recursively returned NULL w/ err: {srn}")
+        
+class deepsplit:
+    def __init__(self, frame, current_frame_count, processed_path):
+        self.frame = frame
+        self.current_frame_count = current_frame_count
+        self.processed_path = processed_path
+        self.queue
+        self.red
+        self.green
+        self.blue
+        self.gray
 
+    def deepSplit_processed(self):
+        try:
+            self.frame = cvtColor(self.frame, COLOR_RGB2BGR)
+
+            self.queue = self.frame.copy()
+
+            self.blue,self.green,self.red = split(self.queue)
+
+            self.gray = cvtColor(self.queue, COLOR_BGR2GRAY)
+
+            file_name=f"split_frame_{self.current_frame_count}"
+
+            file_write(self.processed_path, file_name, self.red,self.green,self.blue,self.gray, self.frame)
+
+            print(f"Frame {self.current_frame_count} processed successfully")
+
+        except Exception as e:
+            error(f"Error processing frame {self.current_frame_count}: {e}")
+            raise processingNullFrames(f"Error processing frame {self.current_frame_count}: {e}")
