e0dbf3d2721a47f436c737c22d8958684de753f9
BabyWaffles <57724817+BabyWaffles@users.noreply.github.com>
2024-04-19 14:44:17 +0800

    pushed framesplit_v0.0.9.py

--- Split_frames.py
+++ Split_frames.py
@@ -1,96 +1,118 @@
-import cv2
-import numpy as np
-import os
-import logging
-import threading
-import asyncio
+from cv2 import cvtColor, COLOR_RGB2BGR, split, COLOR_BGR2GRAY, imwrite, imread, VideoCapture, destroyAllWindows
+from os import makedirs, getcwd, path, listdir
+from logging import info, error
+from threading import Thread
+from concurrent.futures import ThreadPoolExecutor
+from asyncio import gather, get_event_loop
+from time import time
 
-current_frame_count = 0
+print("Initializing Frame Splitter...")
+start = time()
 
-def new_directory(folder_name):
-    try:
-        current_directory = os.getcwd()
-        join_dir = os.path.join(current_directory, folder_name)
-        if not os.path.exists(join_dir):
-            os.makedirs(join_dir)
-            logging.info(f"Processed img directory {folder_name} created, in {current_directory}")
-            return join_dir
-        else:
-            logging.info("Directory already exists")
-            return join_dir
-    except os.error as e:
-        logging.error(f"Error creating directory: {e}")
-        return None
-    except Exception as e:
-        logging.error(f"Recursively returned NULL w/ err: {e}")
-        return "NULL"
+class systemRecurrsiveNull:
+    pass
 
-data_folder = f"{os.getcwd()}\cvat-dataInference"
-#processed_path = f"{data_folder}\processed_imgs" #f"{new_directory("cvat-dataInference\processed_imgs")}"
+class processingNullFrames:
+    pass
 
-class deepsplitting:
-    def deepSplit_processed(frame, current_frame_count, processed_path):
+class global_framecount:
+    def __init__(self):
+        self.framecount = 0
+    def up_framecount(self):
+        self.framecount += 1
+        return self.framecount
+
+class GetCWD:
+    def __init__(self, folder_name):
+        self.fname = folder_name
+        self.dir = getcwd()
+    
+    def newdir(self):
         try:
-            frames= cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            current_frame_queue = frames.copy()
-            b,g,r = cv2.split(current_frame_queue)
-            #convert to gray
-            gray = cv2.cvtColor(current_frame_queue, cv2.COLOR_BGR2GRAY)
-            file_name=f"split_frame_{current_frame_count}"
-            file_write(processed_path, file_name, r,g,b,gray, frame)
-            print(f"Frame {current_frame_count} processed successfully")
-        except Exception as e:
-            print(f"Error processing frame {current_frame_count}: {e}")
-
-
-class threadings: 
-    def worker_threads(data_folder, current_frame_count):
-        #asyncio.gather(call_class_deepsplit(data_folder, current_frame_count))
-        call_class_deepsplit(data_folder, current_frame_count)
-    async def workthreading(data_folder, current_frame_count):
-        threads = []
-        print(f"Starting worker thread with: {threads} threads")
-        for i in range(1):
-            t = threading.Thread(target=threadings.worker_threads, args=(data_folder, current_frame_count))
-            threads.append(t)
-            t.start()
-        for t in threads:
-            t.join()
-        print("Threads completed successfully")
+            self.joindirw =  path.join(self.dir, self.fname)
+            if not path.exists(self.joindirw):
+                info(f"Processed img directory {self.fname} created, in {self.dir}")
+                return makedirs(self.joindirw), 
+            elif self.fname == None:
+                raise systemRecurrsiveNull(f"No directory name provided!")
+            else:
+                return self.joindirw
+        except error as oserr:
+            oserr(f"Error creating directory: {oserr}")	
+            return None
+        except systemRecurrsiveNull as srn:
+            raise systemRecurrsiveNull(f"Recursively returned NULL w/ err: {srn}")
         
+class deepsplit:
+    def __init__(self, frame, current_frame_count, processed_path):
+        self.frame = frame
+        self.current_frame_count = current_frame_count.framecount
+        self.processed_path = processed_path
+        self.queue = None
+        self.red = None
+        self.green = None
+        self.blue = None
+        self.gray = None
+        self.file_name = None
+        self.threadexecutor = ThreadPoolExecutor(max_workers=5)
 
-def file_write(path, file_name, r,g,b,gray, normal):
-    with open(path + file_name, 'w') as f:
-        cv2.imwrite(f"{path}/{file_name}_r.jpg", r)
-        cv2.imwrite(f"{path}/{file_name}_g.jpg", g)
-        cv2.imwrite(f"{path}/{file_name}_b.jpg", b)
-        cv2.imwrite(f"{path}/{file_name}_gray.jpg", gray)
-        cv2.imwrite(f"{path}/{file_name}_normal.jpg", normal)
-        logging.info(f"Frames written successfully {file_name}")
-        f.close()
+    def deepSplit_processed(self):
+        try:
+            self.frame = cvtColor(self.frame, COLOR_RGB2BGR)
+            self.queue = self.frame.copy()
+            self.blue, self.green, self.red = split(self.queue)
+            self.gray = cvtColor(self.queue, COLOR_BGR2GRAY)
+            self.file_name=f"split_frame_{self.current_frame_count}"
+            self.file_processing()
+            print(f"Frame {self.current_frame_count} processed successfully")
 
-def call_class_deepsplit(folder_path, current_frame_count):
-    items = os.listdir(folder_path)
-    files = [item for item in items if os.path.isfile(os.path.join(folder_path, item))]
-    logging.info(f"Files in directory: {files}")
-    processed_path = new_directory(f"{folder_path}\processed_imgs")
+        except Exception as e:
+            error(f"Error processing frame {self.current_frame_count}: {e}")
+            #raise processingNullFrames(f"Error processing frame {self.current_frame_count}: {e}")
+    
+    def file_processing(self):
+        with open(self.processed_path + self.file_name, 'w') as file:
+            imwrite(f"{self.processed_path}/{self.file_name}_r.jpg", self.red)
+            imwrite(f"{self.processed_path}/{self.file_name}_g.jpg", self.green)
+            imwrite(f"{self.processed_path}/{self.file_name}_b.jpg", self.blue)
+            imwrite(f"{self.processed_path}/{self.file_name}_gray.jpg", self.gray)
+            imwrite(f"{self.processed_path}/{self.file_name}_normal.jpg", self.queue)
+            info(f"Frame {self.current_frame_count} processed successfully")
+            file.close()
+            
+    async def async_imwrite(self, filename, img):
+        loop = get_event_loop()
+        await loop.run_in_executor(self.threadexecutor, imwrite, filename, img)
+
+end = time()
+print(f"Frame Splitter initialized successfully with {end-start}ms!")
+
+def lastly(folder_path, current_frame_count):
+    items = listdir(folder_path)
+    files = [item for item in items if path.isfile(path.join(folder_path, item))]
+    info(f"Files in directory: {files}")
+    processed_path = GetCWD(f"{folder_path}\processed_imgs").newdir()
     print(f"Processed path: {processed_path}")
     for file in files:
         if file.endswith((".jpg", ".png", ".jpeg", ".tiff", ".bmp")):
-            logging.info(f"Processing file: {file}")
-            img = cv2.imread(f"{folder_path}/{file}")   
-            deepsplitting.deepSplit_processed(img, current_frame_count, processed_path)
-            current_frame_count += 1
-        elif file.endswith((".mp4",".mkv", ".avi", ".gif", ".wmv", ".flv", ".mov", ".webm")):
-            logging.info(f"Processing video: {file}")
-            cap = cv2.VideoCapture(f"{folder_path}/{file}")
+            info(f"Processing file: {file}")
+            img = imread(f"{folder_path}/{file}")   
+            deepsplit(img, current_frame_count, processed_path).deepSplit_processed()
+            current_frame_count.up_framecount()
+        elif file.endswith((".avi", ".mp4", ".mov", ".flv")):
+            info(f"Processing video: {file}")
+            cap = VideoCapture(f"{folder_path}/{file}")
             while cap.isOpened():
                 ret, frame = cap.read()
                 if ret:
-                    deepsplitting.deepSplit_processed(frame, current_frame_count, processed_path)
-                    current_frame_count += 1
+                    deepsplit(frame, current_frame_count, processed_path).deepSplit_processed()
+                    current_frame_count.up_framecount()
+                else:
+                    break
             cap.release()
-            cv2.destroyAllWindows()
+            destroyAllWindows()
 
-#call_class_deepsplit(data_folder, current_frame_count)
-asyncio.get_event_loop().run_until_complete(threadings.workthreading(data_folder, current_frame_count))
+data_folder = f"{getcwd()}\cvat-dataInference"
+#processed_path = f"{data_folder}\processed_imgs" #f"{GetCWD("cvat-dataInference\processed_imgs").newdir()}" #Deprecated! Integrated it into Lastly function!
+framecount = global_framecount()
+lastly(data_folder, framecount)
